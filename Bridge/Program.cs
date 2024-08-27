685d88ccb9427038a3c832c23c550c535b9f53e8
LuisER5 <luis.e.rodriguez@accenture.com>
2024-08-27 14:04:37 -0600

    Bridge Pattern

---
+++ Bridge/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Bridge;
+
+Console.Title = "Bridge";
+
+var noCoupon = new NoCoupon();
+var oneEuroCoupon = new OneEuroCoupon();
+
+var meatBasedMenu = new MeatBaseMenu(noCoupon);
+Console.WriteLine($"Meat base menu, no coupon: {meatBasedMenu.CalculatePrice()} euro.");
+
+var vegetarianMenu = new VegetarianMenu(noCoupon);
+Console.WriteLine($"Vegetarian menu, no coupon: {vegetarianMenu.CalculatePrice()} euro");
+
+meatBasedMenu = new MeatBaseMenu(oneEuroCoupon);
+Console.WriteLine($"Meat base menu, no coupon: {meatBasedMenu.CalculatePrice()} euro.");
+
+vegetarianMenu = new VegetarianMenu(oneEuroCoupon);
+Console.WriteLine($"Vegetarian menu, no coupon: {vegetarianMenu.CalculatePrice()} euro");
+
+
+Console.ReadKey();
