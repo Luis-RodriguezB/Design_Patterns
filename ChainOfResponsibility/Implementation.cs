fc1d1e10b3b1d88790b59a65a2af267f5d200812
LuisER5 <luis.e.rodriguez@accenture.com>
2024-09-30 13:12:36 -0600

    Chain of Responsibility Pattern

---
+++ ChainOfResponsibility/Implementation.cs
@@ -0,0 +1,110 @@
+﻿namespace ChainOfResponsibility;
+
+/// <summary>
+/// Handler Interface
+/// </summary>
+public interface ISupportHandler
+{
+    void SetNextHandler(ISupportHandler handler);
+    void HandleTicket(Ticket ticket);
+}
+
+/// <summary>
+/// Abstract base class for concrete handlers
+/// </summary>
+public abstract class SupportHandlerBase : ISupportHandler
+{
+    private ISupportHandler _nextHandler;
+
+    public void SetNextHandler(ISupportHandler handler)
+    {
+        _nextHandler = handler;
+    }
+
+    public virtual void HandleTicket(Ticket ticket)
+    {
+        if (CanHandleTicket(ticket))
+        {
+            Handle(ticket);
+        } 
+        else if(_nextHandler != null)
+        {
+            _nextHandler.HandleTicket(ticket);
+        } 
+        else
+        {
+            Console.WriteLine("Ticket cannot be handle.");
+        }
+    }
+
+    protected abstract bool CanHandleTicket(Ticket ticket);
+    protected abstract void Handle(Ticket ticket);
+}
+
+/// <summary>
+/// Concrete handler: Level 1 Support
+/// </summary>
+public class Level1SupportHandler : SupportHandlerBase
+{
+    protected override bool CanHandleTicket(Ticket ticket)
+    {
+        return ticket.Severity == Severity.Low;
+    }
+
+    protected override void Handle(Ticket ticket)
+    {
+        Console.WriteLine("Level 1 Support handles the ticket.");
+        // Handle the ticket at Level 1 Support
+    }
+}
+
+/// <summary>
+/// Concrete handler: Level 2 Support
+/// </summary>
+public class Level2SupportHandler : SupportHandlerBase
+{
+    protected override bool CanHandleTicket(Ticket ticket)
+    {
+        return ticket.Severity == Severity.Medium;
+    }
+
+    protected override void Handle(Ticket ticket)
+    {
+        Console.WriteLine("Level 2 Support handles the ticket.");
+        // Handle the ticket at Level 2 Support
+    }
+}
+
+/// <summary>
+/// Concrete handler: Level 1 Support
+/// </summary>
+public class Level3SupportHandler : SupportHandlerBase
+{
+    protected override bool CanHandleTicket(Ticket ticket)
+    {
+        return ticket.Severity == Severity.High;
+    }
+
+    protected override void Handle(Ticket ticket)
+    {
+        Console.WriteLine("Level 3 Support handles the ticket.");
+        // Handle the ticket at Level 3 Support
+    }
+}
+
+/// <summary>
+/// Ticket Class
+/// </summary>
+public class Ticket
+{
+    public Severity Severity { get; set; }
+}
+/// <summary>
+/// Enum representing severity levels of tickets
+/// </summary>
+public enum Severity
+{
+    Low,
+    Medium,
+    High
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,32 @@
+﻿using ChainOfResponsibility;
+
+/*
+ The Chain of Responsibility pattern is a behavioral design pattern that allows a request 
+ to be passed along a chain of potential handlers until it finds the appropriate one to process it. 
+ Each handler in the chain has the autonomy to handle the request or delegate it to the next handler. 
+ This decoupled approach promotes flexibility, scalability, and maintainability in workflow management scenarios.
+ */
+
+Console.Title = "Chain of Responsibility Pattern";
+
+// Create the support handlers
+var level3SupportHandler = new Level3SupportHandler();
+var level2SupportHandler = new Level2SupportHandler();
+var level1SupportHandler = new Level1SupportHandler();
+
+// Set the chain of responsibility
+level1SupportHandler.SetNextHandler(level2SupportHandler);
+level2SupportHandler.SetNextHandler(level3SupportHandler);
+
+// Tickets
+var ticket1 = new Ticket { Severity = Severity.Low };
+var ticket2 = new Ticket { Severity = Severity.Medium };
+var ticket3 = new Ticket { Severity = Severity.High };
+
+// Process the tickets
+level1SupportHandler.HandleTicket(ticket1);
+level1SupportHandler.HandleTicket(ticket2);
+level1SupportHandler.HandleTicket(ticket3);
+
+
+Console.ReadKey();
